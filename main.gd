a52c4d94872c3eb62c9108183a47aa5a57056b82
Dillon Steyl <dillon.steyl@tethernaut.com>
2026-01-30 21:07:35 +1000

    restructure for ui-heavy scene tree

--- main.gd
+++ main.gd
@@ -3,8 +3,8 @@ extends Node
 
 const MAIN_MENU_SCENE = preload("uid://cgppg2ly351q0")
 
-@onready var world: Node3D = $World
-@onready var ui: Control = $UI
+@onready var menu: Control = $Menu
+@onready var game: Control = $Game
 
 
 func _ready() -> void:
@@ -12,23 +12,23 @@ func _ready() -> void:
 
 
 func go_to_menu() -> void:
-	set_world(null)
+	set_game(null)
 
 	var main_menu: MainMenu = MAIN_MENU_SCENE.instantiate()
-	set_ui(main_menu)
+	set_menu(main_menu)
 
 
-func set_ui(ui_node: Control) -> void:
-	for child in ui.get_children():
+func set_menu(ui_node: Control) -> void:
+	for child in menu.get_children():
 		child.queue_free()
 
 	if ui_node:
-		ui.add_child(ui_node)
+		menu.add_child(ui_node)
 
 
-func set_world(world_node: Node3D) -> void:
-	for child in world.get_children():
+func set_game(game_node: Node3D) -> void:
+	for child in game.get_children():
 		child.queue_free()
 
-	if world_node:
-		world.add_child(world_node)
+	if game_node:
+		game.add_child(game_node)
