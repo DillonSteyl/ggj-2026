bd852278333459618540a18bf8e8b10a94adfb1d
Dillon Steyl <dillon.steyl@tethernaut.com>
2026-02-01 13:44:39 +1000

    require 5 passes to proceed

--- game/requests/resources/stage.gd
+++ game/requests/resources/stage.gd
@@ -4,5 +4,5 @@ extends Resource
 @export var request_generator: RequestGenerator
 
 @export_group("Success Criteria")
-@export var num_successes: int = 3
-@export var from_x_last_attempts: int = 3
+@export var num_successes: int = 5
+@export var from_x_last_attempts: int = 5
